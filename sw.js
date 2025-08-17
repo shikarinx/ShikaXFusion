e2d0f7748e9065360e9746267cdd7e19547c2b1b
shikarinx <shikarin.xx@gmail.com>
2025-08-17 13:04:49 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,36 @@
-self.addEventListener('fetch', event => {});
+// A simple service worker for caching assets
+
+const CACHE_NAME = 'shikaxfusion-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/home.html',
+  '/login.css',
+  '/home.css',
+  '/login.js',
+  '/main.js',
+  '/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
